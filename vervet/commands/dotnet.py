89d1d0c73c4cdd22f01f4e4032f43fcb0bdb74c4
Dan O'Meara <omeara.dan@gmail.com>
2018-07-01 16:29:40 -0700

    reorder output 'columns'

--- vervet/commands/dotnet.py
+++ vervet/commands/dotnet.py
@@ -14,13 +14,13 @@ SEARCH_TERM = 'Google.Cloud'
 
 def dotnet():
     reg_page_list = nuget_search(SEARCH_TERM)
-    downloads_dir = str(Path.home()) + '/Downloads/'
+    downloads_dir = str(Path.home()) + '/vervet-output/'
     now_str = datetime.now().strftime('%Y%m%d-%H%M%S')
     outfile = now_str + '_dotnet.csv'
-    with open(downloads_dir + outfile , 'w') as csvfile:
+    with open(downloads_dir + outfile, 'w') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['package_name', 'version', 'publish_date', 'listed',
-                         'prerelease', 'language'])
+        writer.writerow(['package_name', 'version', 'prerelease',
+                         'publish_date', 'listed', 'language'])
 
         for reg_page in tqdm(reg_page_list):
             catalog_entry_list = nuget_registration(reg_page)
@@ -44,10 +44,10 @@ def nuget_search(search_term):
     """
     response = requests.get('https://api-v2v3search-0.nuget.org/query?q=' +
                             search_term + '&prerelease=true&take=200')
-    print('\nMaking request to NuGet Search Service...{}'.format(
+    print('\n.NET: making request to NuGet Search Service...{}'.format(
           response.status_code))
     response_json = response.json()
-    print('Got {} hits on search term: "{}".'.format(
+    print('NuGet returned {} hits on search term: "{}".'.format(
           response_json['totalHits'], search_term))
     google_packages = [package['@id'] for package in response_json['data']
                        if 'Google Inc.' in package['authors']]
@@ -64,22 +64,18 @@ def nuget_registration(reg_index_url):
     """
     response = requests.get(reg_index_url)
     response_json = response.json()
-    reg_page_count = response_json['count']
     reg_pages = response_json['items']
     # Refer to NuGet API docs for a discussion on NuGet API registration
     # pages and leaves. The items array is not always provided.
-
     # TODO: when items _not_ in items hit the @id url and navigate that
-
     # TODO: validate assumption that last entry in list is the most recent
     # event
+    
     catalog_entry_list = []
-
     for page in reg_pages:
         if 'items' in page:
             for leaf in page['items']:
                 catalog_entry_list.append(leaf['catalogEntry']['@id'])
-
     return catalog_entry_list
 
 
@@ -108,7 +104,7 @@ def nuget_catalog_entry(catalog_entry_url):
 
     return [response_json['id'],
             response_json['version'],
+            prerelease,
             publish_date,
             listed,
-            prerelease,
             ]
